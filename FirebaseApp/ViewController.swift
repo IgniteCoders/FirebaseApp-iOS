57cc912b727c7594c54b2f203a3fb174156a6214
Mansour Boutarbouch Mhaimeur <theboom.kajas@gmail.com>
2026-02-05 20:48:27 +0100

    Added Firebase dependencies Testing SignIn and SignUp with email and password

--- FirebaseApp/ViewController.swift
+++ FirebaseApp/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUp(_ sender: Any) {
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        Auth.auth().createUser(withEmail: username, password: password) { [unowned self] authResult, error in
+            if let error = error {
+                print("Error creating account: \(error.localizedDescription)")
+                return
+            }
+            
+            print("Account created successfully")
+        }
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        Auth.auth().signIn(withEmail: username, password: password) { [unowned self] authResult, error in
+            if let error = error {
+                print("Error signing in: \(error.localizedDescription)")
+                return
+            }
+            
+            print("Sign in successfully")
+            performSegue(withIdentifier: "NavigateToHome", sender: nil)
+        }
+    }
 }
 
